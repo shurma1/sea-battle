7a692e03276a378eb52342e538d2359b8ff24b72
shurma1 <dmitriy.prokhorov.04@mail.ru>
2024-02-14 23:52:22 +0300

    add types for db scheme

--- backend/src/models/models.ts
+++ backend/src/models/models.ts
@@ -1,39 +1,47 @@
 import sequelize from '../db';
 import {DataTypes} from 'sequelize';
+import {PlayerInstance} from './types/Player';
+import {TokenInstance} from './types/Token';
+import {MatchInstance} from './types/Match';
+import {MoveInstance} from './types/Move';
+import {ParticipantInstance} from './types/Participant';
+import {ShipInstance} from './types/Ship';
 
-const Player = sequelize.define('Player', {
+
+
+const Player = sequelize.define<PlayerInstance>('Player', {
 	id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 	email: {type: DataTypes.STRING, unique: true},
 	password: {type: DataTypes.STRING},
 	mmr: {type: DataTypes.FLOAT, defaultValue: 0.00}
 });
 
-const Token = sequelize.define('Token', {
+const Token = sequelize.define<TokenInstance>('Token', {
 	id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 	//created add auto
 	token: {type: DataTypes.STRING}
 });
 
-const Match = sequelize.define('Match', {
+const Match = sequelize.define<MatchInstance>('Match', {
 	id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 	//created add auto
 	ended: {type: 'TIMESTAMP'}
 });
 
-const Move = sequelize.define('Move', {
-	id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 
+const Move = sequelize.define<MoveInstance>('Move', {
+	id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 	coordinate: {type: DataTypes.STRING},
 	isHit: {type: DataTypes.BOOLEAN}
 });
 
 
-const Participant = sequelize.define('Participant', {
+const Participant = sequelize.define<ParticipantInstance>('Participant', {
 	id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 	isWin: {type: DataTypes.BOOLEAN}
 });
 
-const Ship = sequelize.define('Ship', {
+const Ship = sequelize.define<ShipInstance>('Ship', {
 	id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 	coordinate: {type: DataTypes.STRING},
 });
