2ec7e576ad86bb447482dc0fffb0d5c1d3c121f1
shurma1 <dmitriy.prokhorov.04@mail.ru>
2024-02-14 23:53:56 +0300

    fix

--- backend/src/middleware/ErrorHandling.middleware.ts
+++ backend/src/middleware/ErrorHandling.middleware.ts
@@ -1,15 +1,22 @@
-import {Request, Response} from 'express';
+import {NextFunction, Request, Response} from 'express';
 import {ApiError} from '../error/ApiError';
 
 
-export default (err: Error | ApiError, req: Request, res: Response) => {
+
+const errorHandlingMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
+	console.log(err);
 	if(err instanceof ApiError){
 		return res.status(err.status).json({
 			message: err.message
 		});
 	}
 
+
 	res.status(500).json({
 		message: 'Unknown error!'
 	});
+};
+
+export {
+	errorHandlingMiddleware
 };
