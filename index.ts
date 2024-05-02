eee945ae3c19e0daf446363adc8cefb90ec39ae7
shurma1 <dmitriy.prokhorov.04@mail.ru>
2024-05-02 15:45:02 +0300

    add ws

--- index.ts
+++ index.ts
@@ -2,9 +2,18 @@ import express from 'express';
 import config from 'config';
 import http from 'http';
 import https from 'https';
+import expressWs from 'express-ws';
+import {ErrorHandlingMiddleware} from "./src/middleware/ErrorHandling.middleware";
+import {socket} from "./src/socket";
 
 
-const app = express();
+const expressApp = express();
+
+const {app} = expressWs(expressApp);
+
+app.use(express.json());
+
+app.ws('/', socket);
 
 
 const PORT = config.get('server.PORT') as number || 3000;
@@ -16,6 +25,6 @@ const isDev = config.get('isDev') as boolean;
 app.listen(
 	PORT,
 	() => {
-		console.log('start')
+		console.log(`start http://localhost:${PORT}`);
 	}
 )
