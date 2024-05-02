708f3fea5c835b6efc7d2e71d941a3d05eac3510
shurma1 <dmitriy.prokhorov.04@mail.ru>
2024-05-02 15:43:17 +0300

    TYPES: add answer type GameFound

--- src/types/Answers/GameFound.ts
+++ src/types/Answers/GameFound.ts
@@ -1,6 +1,12 @@
 import {AnswerType} from "../AnswerType";
+import {Player} from "../Player";
+import {PlayerDTO} from "../../dtos/Player";
 
 export interface GameFound {
     type: AnswerType.GameFound;
-    data:
+    data: {
+        isYourMove: boolean,
+        gameId: string,
+        enemy: PlayerDTO
+    };
 }
